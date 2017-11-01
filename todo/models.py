1534950f956a912478cee7ea66931b1ea9bcebf2
Koki Moribe <koki.moribe+coding@gmail.com>
2017-10-31 18:42:10 -0700

    add Board model and add Task.board_id and Task.number columns

--- todo/models.py
+++ todo/models.py
@@ -2,12 +2,12 @@
 # pylint: disable=invalid-name
 # pylint: disable=too-few-public-methods
 
-from sqlalchemy import Column, Integer, String, DateTime, func, MetaData
+from sqlalchemy import Column, Integer, String, DateTime, func, ForeignKey, MetaData
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 from todo.database import session
-from todo.enums import Status
-
+from todo.constants import TO_DO
 
 # Define convention for naming constraints
 # This convention will be used by Alembic when new constraints are added
@@ -26,23 +26,48 @@ Base.metadata = MetaData(naming_convention=convention)
 Base.query = session.query_property()
 
 
+class Board(Base):
+    """Board model"""
+    __tablename__ = 'boards'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String, nullable=False)
+    task_number = Column(Integer, nullable=False)
+    created_at = Column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at = Column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+        nullable=False)
+    tasks = relationship('Task', backref='board')
+
+    def __init__(self, name):
+        self.name = name
+        self.task_number = 0
+
+
 class Task(Base):
     """Task model"""
     __tablename__ = 'tasks'
 
     id = Column(Integer, primary_key=True, nullable=False)
+    number = Column(Integer, nullable=False)
     title = Column(String, nullable=False)
     description = Column(String, nullable=False)
     status = Column(String, nullable=False)
-    created_at = Column(DateTime(timezone=True),
-                        server_default=func.now(),
-                        nullable=False)
-    updated_at = Column(DateTime(timezone=True),
-                        server_default=func.now(),
-                        onupdate=func.now(),
-                        nullable=False)
+    created_at = Column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at = Column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+        nullable=False)
+    board_id = Column(Integer, ForeignKey('boards.id'))
 
-    def __init__(self, title, description, status=Status.TO_DO.name):
+    def __init__(self, number, title, description, board_id, status=TO_DO):
+        self.number = number
         self.title = title
         self.description = description
         self.status = status
+        self.board_id = board_id
