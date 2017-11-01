710d39d55f25c08ccfeeb43cecd7533341bb5c9b
Koki Moribe <koki.moribe+coding@gmail.com>
2017-10-31 18:44:52 -0700

    change default value of `DEBUG` config to `True`

--- todo/config.py
+++ todo/config.py
@@ -3,9 +3,8 @@
 import os
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = os.environ.get('DEBUG', True)
 HOST = os.environ.get('HOST', 'localhost:9090')
-
 
 if not DATABASE_URL:
     raise Exception('Please set `DATABASE_URL` environment variable.')
