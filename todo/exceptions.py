255357cf15f657325afab8eac57f17ec4c9a66ce
Koki Moribe <koki.moribe+coding@gmail.com>
2017-11-02 20:13:17 -0700

    add `UnauthorizedError` exception

--- todo/exceptions.py
+++ todo/exceptions.py
@@ -2,8 +2,17 @@
 
 
 class NotFoundError(Exception):
-    """Exception for Not Found error"""
+    """Exception for HTTP Not Found error"""
     status_code = 404
+
+    def to_dict(self):
+        """Convert exception to dictionary representation"""
+        return {'error': str(self), 'status': self.status_code}
+
+
+class UnauthorizedError(Exception):
+    """Exception for HTTP Unauthorized error"""
+    status_code = 401
 
     def to_dict(self):
         """Convert exception to dictionary representation"""
